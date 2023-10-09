9358ea3c780ecd4c9436a1b24229eb0b4bd4ab08
Cstm Dev <cstm@dev.com>
2023-10-09 21:53:44 +0200

    Add styled components

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import App from "features/App.jsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
